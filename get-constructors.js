fd055dff349a7bc7e8ead182f40d7c1583b93eaf
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-03-23 13:09:08 +0900

    support web browsers

--- get-constructors.js
+++ get-constructors.js
@@ -3,23 +3,33 @@
 (function () {
   'use strict';
 
-  module.exports = exports = constructors;
+  if (typeof module === 'object' && typeof module.exports === 'object')
+    module.exports = exports = constructors;
+  else
+    Function('return this')()['constructors'] = constructors;
 
   var getProto = Object.getPrototypeOf ? Object.getPrototypeOf :
     function getProto(obj) { return obj.__proto__ };
 
+  // constructors
   function constructors(obj) {
+    // supports: getter and normal function
     if (arguments.length === 0) obj = this;
+
+    // convert to object from primitives
     if (obj != null && typeof obj !== 'object' && typeof obj !== 'function')
       obj = Object(obj);
+
     var classes = [];
 
     if (obj instanceof Function) {
+      // for Class/constructor
       for (; obj; obj = getProto(obj))
         if (typeof obj === 'function')
           classes.push(obj);
     }
     else {
+      // for instance/object
       for (; obj; obj = getProto(obj))
         if (obj.hasOwnProperty('constructor'))
           classes.push(obj.constructor);
@@ -28,6 +38,7 @@
     return classes;
   }
 
+  // extendPrototype
   constructors.extendPrototype = function extendPrototype(Class) {
     Class = Class || Object;
     if (!Object.prototype.hasOwnProperty('constructors')) {
